407259cd9d0c05afc0869783b1cd1a042966e636
matxinn03 <matxinj@gmail.com>
2022-04-07 16:25:18 +0200

    I've added some comments in order to make the code more understandable

--- source/timer.c
+++ source/timer.c
@@ -12,7 +12,7 @@ void timer_UpdateTimer()
     // Establecemos, 512 ticks - ~1 segundo.
     timer.ticks++;
     iprintf("\x1b[11;00H ticks: %i", timer.ticks);
-    if(timer.ticks == TIMER0_FREQ){
+    if(timer.ticks == TIMER0_FREQ){ //TIMER0_FREQ = 512
         timer.time++; // Seconds++
         timer.ticks = 0;
     }
@@ -24,7 +24,7 @@ void timer_ConfigureTimer(int latch, int mask)
 
     timer.ticks = 0;
     timer.interruptionRate = 0;
-    timer.latch = latch;
+    timer.latch = latch;    //latch = 65.536 - (1/(ticks/seg)) * (33.554.432/x); x = bits 1-0 del registro TIMER0_CNT(Ir probando hasta obtener);
     timer.conf = mask;
     timer.time = 0;
 
@@ -34,22 +34,22 @@ void timer_ConfigureTimer(int latch, int mask)
 
 void timer_EnableInterruptions(){
     IME = 0;
-    IE |= IRQ_TIMER0;
+    IE |= IRQ_TIMER0;   //  |= es un Or bit a bit; IRQ_TIMER0 = BIT(3); Se pone a 1 el bit correspondiente en el registro IE
     IME = 1;
 }
 
 void timer_DisableInterruptions(){
     IME = 0;
-    IE &= ~IRQ_TIMER0;
+    IE &= ~IRQ_TIMER0;  //  &= es un And bit a bit; IRQ_TIMER0 = BIT(3); Se pone a 0 el bit correspondiente en el registro IE
     IME = 1;
 }
 
 void timer_StartTimer()
 {
-    TIMER0_CNT |= BIT(7);
+    TIMER0_CNT |= BIT(7);   //Poner a 1 el bit 7 del registro TIMER0_CNT activa el temporizador
 }
 
 void timer_StopTimer()
 {
-    TIMER0_CNT &= ~BIT(7);
+    TIMER0_CNT &= ~BIT(7);  //Poner a 0 el bit 7 del registro TIMERO_CNT desactiva el temporizador
 }
